2510efe0bb3335cb963d1f867dc06a11a5e7abc9
Jan Opitz <jopitz@hm.edu>
2014-11-19 14:14:52 +0100

    Extract MissingIcon

---
+++ src/main/java/cs/hm/edu/sam/mc/misc/MissingIcon.java
@@ -0,0 +1,49 @@
+package cs.hm.edu.sam.mc.misc;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import javax.swing.Icon;
+
+/**
+ * Icon class that creates an default icon when missing.
+ */
+public class MissingIcon implements Icon {
+
+    private final int width = 32;
+    private final int height = 32;
+
+    private final BasicStroke stroke = new BasicStroke(4);
+
+    @Override
+    public void paintIcon(final Component c, final Graphics g, final int x, final int y) {
+        final Graphics2D g2d = (Graphics2D) g.create();
+
+        g2d.setColor(Color.WHITE);
+        g2d.fillRect(x + 1, y + 1, width - 2, height - 2);
+
+        g2d.setColor(Color.BLACK);
+        g2d.drawRect(x + 1, y + 1, width - 2, height - 2);
+
+        g2d.setColor(Color.RED);
+
+        g2d.setStroke(stroke);
+        g2d.drawLine(x + 10, y + 10, (x + width) - 10, (y + height) - 10);
+        g2d.drawLine(x + 10, (y + height) - 10, (x + width) - 10, y + 10);
+
+        g2d.dispose();
+    }
+
+    @Override
+    public int getIconWidth() {
+        return width;
+    }
+
+    @Override
+    public int getIconHeight() {
+        return height;
+    }
+}
