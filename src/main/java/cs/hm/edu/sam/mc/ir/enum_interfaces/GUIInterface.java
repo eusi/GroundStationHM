b4e9abbb85895955fdf279cf0085b577d59c6814
rolander <rwidmann@hm.edu>
2014-11-19 12:44:32 +0100

    modified:   src/main/java/cs/hm/edu/sam/mc/ir/Ir.java 	modified:   src/main/java/cs/hm/edu/sam/mc/ir/enum_interfaces/GUIInterface.java 	modified:   src/main/java/cs/hm/edu/sam/mc/ir/enum_interfaces/TasksEnum.java

--- src/main/java/cs/hm/edu/sam/mc/ir/enum_interfaces/GUIInterface.java
+++ src/main/java/cs/hm/edu/sam/mc/ir/enum_interfaces/GUIInterface.java
@@ -3,5 +3,6 @@ package cs.hm.edu.sam.mc.ir.enum_interfaces;
 public interface GUIInterface {
 	
 	public void printConsole(String text);
+	public void setDroneConnectionColor(TasksEnum setColor);
 
 }
